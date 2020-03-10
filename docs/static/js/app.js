8bec88dc05d4267a9ae852d98b710d09ed35479f
jljones78 <jljones78@gmail.com>
2020-03-10 16:19:01 -0500

    add docs directory to deploy github page

---
+++ docs/static/js/app.js
@@ -0,0 +1,26 @@
+
+const handler = function () {
+
+    d3.event.preventDefault()
+    const table = d3.select("table")
+
+    d3.select("tbody").remove()
+    const tbody = table.append('tbody')
+
+    data.filter(dataRow => dataRow.datetime === input.property('value')).forEach(dataRow => {
+        let row = tbody.append("tr")
+        row.append("td").text(dataRow.datetime)
+        row.append("td").text(dataRow.city)
+        row.append("td").text(dataRow.state)
+        row.append("td").text(dataRow.country)
+        row.append("td").text(dataRow.shape)
+        row.append("td").text(dataRow.durationMinutes)
+        row.append("td").text(dataRow.comments)
+    })
+}
+
+const input = d3.select("input")
+const button = d3.select("button")
+
+input.on("change", handler)
+button.on("click", handler)
